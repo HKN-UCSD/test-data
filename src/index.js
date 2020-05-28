ffd017d98307e1448e5df379f0f1b13a994225b3
Thai <ttgilles@ucsd.edu>
2020-05-28 13:13:34 -0700

    Updated randomization for startDate and endDate so they are better suited for testing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import admin from 'firebase-admin';
-import Config from '../config.js';
-import faker from 'faker';
+import admin from "firebase-admin";
+import Config from "../config.js";
+import faker from "faker";
+import moment from "moment";
 
 const EVENTS_COUNT = 50;
-const NAMES_COUNT = 8
-const TAGS_CHOICES = 3
+const NAMES_COUNT = 4;
+const TAGS_CHOICES = 3;
+const HOUR_INCR_MAX = 3;
+const DAY_INCR_MAX = 16;
+const MONTH_INCR_MAX = 4;
 
-const generateNames = () =>
-{
+const generateNames = () => {
   let numIters = Math.floor(Math.random() * NAMES_COUNT) + 1;
   let i = 0;
   let names = [];
@@ -18,40 +21,63 @@ const generateNames = () =>
   }
 
   return names;
-}
+};
 
-const generateTags = () =>
-{
+const generateTags = () => {
   let tags = [];
 
   let randChoice = Math.floor(Math.random() * TAGS_CHOICES);
   switch (randChoice) {
     case 0:
-      tags.push('Professional');
+      tags.push("Professional");
       break;
     case 1:
-      tags.push('Technical');
+      tags.push("Technical");
       break;
-    case 2:
-      tags.push('Social');
+    default:
+      tags.push("Social");
       break;
   }
 
   return tags;
-}
+};
 
-const generateEvents = () =>
-{
+const generateEvents = () => {
   let events = [];
 
   for (let i = 0; i < EVENTS_COUNT; i++) {
+
+    /* 
+     * startDate and endDate of events have randomized offset from the most recent time
+     * this script is run, with the offset for Month being from 0 to 3, Date being from 0 - 15.
+     * Both startDate and endDate should either be on the same day and month, or be 1 or 2 days 
+     * apart from each other. 
+     * 
+     * Furthermore, the hour offset between startDate and endDate is also randomized, with 
+     * the offset being between 1 and 3. 
+     */
+    let dateIncr = Math.floor(Math.random() * DAY_INCR_MAX);
+    let monthIncr = Math.floor(Math.random() * MONTH_INCR_MAX);
+    let startDate = moment(faker.date.recent());
+    let startDateMoment = moment(startDate);
+    startDate = startDateMoment.month(startDateMoment.month() + monthIncr).date(startDateMoment.date() + dateIncr);
+
+    let hourIncr = Math.floor(Math.random() * HOUR_INCR_MAX) + 1;
+    let endDate = moment(startDate).hour(moment(startDate).hour() + hourIncr);
+
     let eventObj = {
-      startDate: faker.date.recent(),
-      endDate: faker.date.future(),
+      startDate: startDate,
+      endDate: endDate,
       hosts: generateNames(),
-      location: faker.address.streetAddress() + ", " + faker.address.city() + ", " + faker.address.country(),
+      location:
+        faker.address.streetAddress() +
+        ", " +
+        faker.address.city() +
+        ", " +
+        faker.address.country(),
       name: faker.random.words(),
       tags: generateTags(),
+      description: faker.lorem.paragraph(),
       urls: {
         rsvp: faker.internet.url(),
         signin: faker.internet.url(),
@@ -64,23 +90,22 @@ const generateEvents = () =>
   }
 
   return events;
-}
+};
 
-const main = () =>
-{
+const main = () => {
   admin.initializeApp({
     credential: admin.credential.cert(Config.FirebaseConfig),
     databaseURL: Config.FirebaseURL,
   });
 
-  let db = admin.firestore()
+  let db = admin.firestore();
   const eventsTestData = generateEvents();
 
   for (let i = 0; i < eventsTestData.length; i++) {
-    db.collection('events').add(eventsTestData[i])
+    db.collection("events").add(eventsTestData[i]);
   }
 
-  console.log('Test data seeded.' + '\n');
-}
+  console.log("Test data seeded." + "\n");
+};
 
 main();
